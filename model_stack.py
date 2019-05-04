6238e7151a295dc44f6d0fba4a4c5ad8dd0bfc89
Mario Rugeles <mrugeles@gmail.com>
2019-05-04 11:40:22 -0500

    add epochs to fit method

--- model_stack.py
+++ model_stack.py
@@ -29,7 +29,7 @@ def fit_model(model, train_tensors, train_targets, valid_tensors, valid_targets,
             verbose=0)
   return model
 
-def fit_model_with_generators(model, train_generator, validation_generator, class_weights):
+def fit_model_with_generators(model, epochs, train_generator, validation_generator, class_weights):
     from keras.optimizers import SGD
     model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
     batch_size = 16
@@ -39,7 +39,7 @@ def fit_model_with_generators(model, train_generator, validation_generator, clas
             train_generator,
             class_weight = class_weights,
             steps_per_epoch=2000 // batch_size,
-            epochs=200,
+            epochs=epochs,
             validation_data=validation_generator,
             validation_steps=800 // batch_size,
           callbacks=[checkpointer], verbose=1)
